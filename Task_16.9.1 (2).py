c7c74e806f3382b14580b3108ab72668981b7235
Raffa1989 <con.t2012@yandex.ru>
2021-11-30 15:50:24 +0300

    Задание 16.9.1

--- Task_16.9.1 (2).py
+++ Task_16.9.1 (2).py
@@ -1,5 +1,5 @@
 from Task_16 import Rectangle, Square
-rect_1 = Rectangle(3,4,7,5)
+rect_1 = Rectangle(3, 4, 7, 5)
 print(rect_1)
 
 square_1 = Square(11)
--- Task_16.py
+++ Task_16.py
@@ -1,14 +1,17 @@
 class Rectangle:
-    def __init__(self,x,y,width,height):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+
     def __str__(self):
         return f'Rectangle({self.x}, {self.y}, {self.width}, {self.height})'
 
+
 class Square:
-    def __init__(self,a):
+    def __init__(self, a):
         self.a = a
+
     def __str__(self):
         return f'Square({self.a})'
